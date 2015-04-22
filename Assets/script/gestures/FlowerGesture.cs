a9e55c6fccd6866012b0a47194218609107e82dc
ww342 <wuwenjie@n2-105-88.dhcp.drexel.edu>
2015-04-22 11:07:46 -0400

    Remove unused code from Flower

--- Assets/script/gestures/FlowerGesture.cs
+++ Assets/script/gestures/FlowerGesture.cs
@@ -7,28 +7,11 @@ public class FlowerGesture : Gesture {
 		this.count++;
 		Sounds.audiosource.PlayOneShot (Sounds.flower);		
 		if (this.count == 4) {
-			this.wrongcount = 0;
 			Sounds.Ambience_A.Stop ();
 			Sounds.Ambience_A.minDistance = 8;
 			Sounds.Ambience_A.clip = Sounds.wave;
 			Sounds.Ambience_D.PlayOneShot (Sounds.landonflpping, 5.0f);
 			Sounds.Ambience_D.PlayOneShot (Sounds.birdstandonpaddle);
-		}
-		// TODO: previously allowed: more than 4 flower gestures.
-		// currently impossible, need to change GameLogic for that to be possible again!
-		if (this.count == 5) {
-			Sounds.audiosource.PlayOneShot (Sounds.leave);
-		}
-		if (this.count == 7) {
-			Sounds.Ambience_D.PlayOneShot (Sounds.leave);
-		}
-		if (this.count == 10) {
-			Sounds.Ambience_D.PlayOneShot (Sounds.leave);
-			Sounds.Ambience_D.PlayOneShot (Sounds.wind, 5.0f);
-		}
-		if (this.count == 12) {
-			//LevelCount ();
-			this.wrongcount = 0;
 		}
 	}
 	
