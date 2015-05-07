fa359aeafe855c3a2f996bb8d2a0147acc9e2952
ww342 <wuwenjie@WuWenjies-MacBook-Air.local>
2015-05-07 02:42:29 -0400

    We have two versions now!

--- Assets/script/GameLogic.cs
+++ Assets/script/GameLogic.cs
@@ -38,9 +38,12 @@ public class GameLogic : MonoBehaviour {
 		yield return StartCoroutine(CheckBareHands(barehands));
 
 		Debug.Log ("Story Begins!");
-		yield return Narrator.PlayAndWait(Narrator.begin);
+		yield return Narrator.PlayAndWait(Narrator.StoneIntro);
 
 		Debug.Log ("Stone throwing");
+		if (GameVersion == 1) {
+						yield return Narrator.PlayAndWait (Narrator.StoneGesture);
+				}
 		Gesture stonethrow = gameObject.AddComponent<StoneThrowGesture>();
 		yield return StartCoroutine(DoStoneThrowing(stonethrow));
 		bool doFlowerGesture = (stonethrow.wrongcount <= 2);
@@ -49,9 +52,10 @@ public class GameLogic : MonoBehaviour {
 		if (doFlowerGesture) {
 			Debug.Log ("Flower petal picking (optional reward)");
 			yield return StartCoroutine(DoFlowerPicking());
+
 		} else {
 			Debug.Log ("NO Flower petal picking (optional reward)");
-			yield return Narrator.PlayAndWait(Narrator.stone3);
+			yield return Narrator.PlayAndWait(Narrator.BirdIntro_afterStone);
 		}
 
 		// example code for stopping here:
@@ -61,6 +65,9 @@ public class GameLogic : MonoBehaviour {
 		yield return StartCoroutine(DoBirdCatching());
 
 		Debug.Log ("Paddle rowing");
+		if (GameVersion == 1) {
+						yield return Narrator.PlayAndWait (Narrator.PaddleGesture);
+				}
 		PaddleRowingGesture paddlerowing = gameObject.AddComponent<PaddleRowingGesture>();
 		yield return StartCoroutine(DoPaddleRowing(paddlerowing));
 		bool doTreeGesture = (paddlerowing.wrongcount <= 2);
@@ -72,7 +79,8 @@ public class GameLogic : MonoBehaviour {
 		} else {
 			Debug.Log ("NO Tree shaking (optional reward)");
 			yield return new WaitForSeconds(5f);
-			yield return Narrator.PlayAndWait(Narrator.ropepose);
+			yield return Narrator.PlayAndWait(Narrator.RopeIntro_afterPaddle);
+
 		}
 
 		Debug.Log ("Rope binding");
@@ -110,35 +118,41 @@ public class GameLogic : MonoBehaviour {
 	}
 
 	IEnumerator DoStoneThrowing(Gesture stonethrow) {
+
 		while (stonethrow.count < 1) {
 			yield return StartCoroutine(stonethrow.Activate());
 		}
 		Sounds.Ambience_A.minDistance = 6;
-		yield return Narrator.PlayAndWait(Narrator.stone1);
+		yield return Narrator.PlayAndWait(Narrator.Stone_Correct_response_01);
 		while (stonethrow.count < 2) {
 			yield return StartCoroutine(stonethrow.Activate());
 		}
 		Sounds.Ambience_A.minDistance = 2;
-		yield return Narrator.PlayAndWait(Narrator.stone2);
+		yield return Narrator.PlayAndWait(Narrator.Stone_Correct_response_02);
 		while (stonethrow.count < 3) {
 			yield return StartCoroutine(stonethrow.Activate());
 		}
 		Sounds.Ambience_A.minDistance = 0;
+		yield return Narrator.PlayAndWait(Narrator.Stone_Correct_response_03);
 	}
 
 	IEnumerator DoFlowerPicking() {
-		yield return Narrator.PlayAndWait(Narrator.flowerpose);
+		yield return Narrator.PlayAndWait(Narrator.FlowerIntro);
+		if (GameVersion == 1) {
+						yield return Narrator.PlayAndWait (Narrator.FlowerGesture);
+				}
 		Gesture flower = gameObject.AddComponent<FlowerGesture>();
 
 		while (flower.count < 1) {
 			yield return StartCoroutine(flower.Activate());
 		}
-		yield return Narrator.PlayAndWait(Narrator.flower1);
+		yield return Narrator.PlayAndWait(Narrator.Flower_Correct_response_01);
 		while (flower.count < 4) {
 			yield return StartCoroutine(flower.Activate());
 		}
+		yield return Narrator.PlayAndWait(Narrator.BirdIntro_afterFlower);
 		Destroy (flower);
-		yield return Narrator.PlayAndWait(Narrator.flyfromtree);
+	
 		// TODO: the above narration (flyfromtree) and the narration in the "else" below
 		// (stone3) that's played instead of the flower gesture overlap!
 		// ideally we would split the instructions at the end from the two response parts
@@ -148,22 +162,26 @@ public class GameLogic : MonoBehaviour {
 	}
 
 	IEnumerator DoBirdCatching() {
+		if (GameVersion == 1) {
+						yield return Narrator.PlayAndWait (Narrator.BirdGesture);
+				}
 		BirdCatchGesture birdcatch = gameObject.AddComponent<BirdCatchGesture>();
 		birdcatch.StartHands(); // separate hands in parallel!
+	
 		while (birdcatch.count < 1) {
 			yield return StartCoroutine(birdcatch.Activate());
 			if (birdcatch.wrongcount > 0) {
-				yield return Narrator.PlayAndWait(Narrator.birdwrong);
+				//yield return Narrator.PlayAndWait(Narrator.birdwrong);
 				birdcatch.wrongcount = 0;
 			}
 		}
-		yield return Narrator.PlayAndWait(Narrator.bird1);
+		yield return Narrator.PlayAndWait(Narrator.Bird_Correct_response_01);
 		while (birdcatch.count < 2) {
 			yield return StartCoroutine(birdcatch.Activate());
 		}
 		birdcatch.StopHands();
 		Destroy (birdcatch);
-		yield return Narrator.PlayAndWait(Narrator.bird2);
+		yield return Narrator.PlayAndWait(Narrator.Bird_Correct_response_02);
 	}
 
 	IEnumerator DoPaddleRowing(PaddleRowingGesture paddlerowing) {
@@ -171,45 +189,53 @@ public class GameLogic : MonoBehaviour {
 		while (paddlerowing.count < 1) {
 			yield return StartCoroutine(paddlerowing.Activate());
 		}
-		yield return Narrator.PlayAndWait(Narrator.paddle1);
+		yield return Narrator.PlayAndWait(Narrator.Paddle_Correct_response_01);
 		while (paddlerowing.count < 4) {
 			yield return StartCoroutine(paddlerowing.Activate());
 		}
 		paddlerowing.StopHands();
-		yield return Narrator.PlayAndWait(Narrator.paddle4);
+		yield return Narrator.PlayAndWait(Narrator.Paddle_Correct_response_03);
 	}
 
 	IEnumerator DoTreeShaking() {
-		yield return Narrator.PlayAndWait(Narrator.treepose);
+		if (GameVersion == 1) {
+						yield return Narrator.PlayAndWait (Narrator.TreeGesture);
+				}
 		Gesture treeshake = gameObject.AddComponent<TreeShakingGesture>();
 		while (treeshake.count < 8) {
 			yield return StartCoroutine(treeshake.Activate());
 		}
 		Destroy (treeshake);
-		yield return Narrator.PlayAndWait(Narrator.tietheboat);
+		yield return Narrator.PlayAndWait(Narrator.RopeIntro_afterTree);
 	}
 
 	IEnumerator DoRopeBinding() {
+		if (GameVersion == 1) {
+						yield return Narrator.PlayAndWait (Narrator.RopeGesture);
+				}
 		Gesture ropebind = gameObject.AddComponent<RopeBindingGesture>();
 		while (ropebind.count < 1) {
 			yield return StartCoroutine(ropebind.Activate());
 		}
-		yield return Narrator.PlayAndWait(Narrator.rope1);
+		yield return Narrator.PlayAndWait(Narrator.Rope_Correct_response_01);
 		while (ropebind.count < 2) {
 
 			yield return StartCoroutine(ropebind.Activate());
 		}
-		yield return Narrator.PlayAndWait(Narrator.rope2);
+		yield return Narrator.PlayAndWait(Narrator.Rope_Correct_response_02);
 		while (ropebind.count < 3) {
 
 			yield return StartCoroutine(ropebind.Activate());
 		}
 		Destroy (ropebind);
-		yield return Narrator.PlayAndWait(Narrator.rope3);
+		yield return Narrator.PlayAndWait(Narrator.Rope_Correct_response_03);
 		Sounds.ChangeBackgroundMusic ();
 	}
 
 	IEnumerator DoBikeRiding() {
+		if (GameVersion == 1) {
+						yield return Narrator.PlayAndWait (Narrator.BikeGesture);
+				}
 		BikeRidingGesture bikeride = gameObject.AddComponent<BikeRidingGesture>();
 		Sounds.RestartBackgroundMusic ();
 		bikeride.StartHands(); // separate hands in parallel!
@@ -218,24 +244,27 @@ public class GameLogic : MonoBehaviour {
 		}
 		bikeride.StopHands();
 		Destroy (bikeride);
-		yield return Narrator.PlayAndWait(Narrator.bell6);
+		yield return Narrator.PlayAndWait(Narrator.StarIntro);
 	}
 
 	IEnumerator DoStarPicking() {
+		if (GameVersion == 1) {
+						yield return Narrator.PlayAndWait (Narrator.StarGesture);
+				}
 		Gesture starpick = gameObject.AddComponent<StarPickingGesture>();
 		while (starpick.count < 1) {
 			yield return StartCoroutine(starpick.Activate());
 		}
-		yield return Narrator.PlayAndWait(Narrator.star1);
+		yield return Narrator.PlayAndWait(Narrator.Star_Correct_response_01);
 		while (starpick.count < 2) {
 			yield return StartCoroutine(starpick.Activate());
 		}
-		yield return Narrator.PlayAndWait(Narrator.star2);
+		yield return Narrator.PlayAndWait(Narrator.Star_Correct_response_02);
 		while (starpick.count < 3) {
 			yield return StartCoroutine(starpick.Activate());
 		}
 		Destroy (starpick);
-		yield return Narrator.PlayAndWait(Narrator.star3);
+		yield return Narrator.PlayAndWait(Narrator.Star_Correct_response_03);
 	}
 	
 }
--- Assets/script/Narrator.cs
+++ Assets/script/Narrator.cs
@@ -4,6 +4,7 @@ using System.Collections;
 [RequireComponent(typeof(AudioSource))]
 public class Narrator : MonoBehaviour
 {
+	/*
 	public AudioClip begin;
 	public AudioClip flowerpose;
 	public AudioClip flower1;
@@ -30,7 +31,67 @@ public class Narrator : MonoBehaviour
 	public AudioClip starwrong;
 	public AudioClip stonewrong;
 	public AudioClip birdwrong;
+*/
 
+	// Game Story Narration
+	public AudioClip StoneIntro;
+	public AudioClip Stone_Correct_response_01;
+	public AudioClip Stone_Correct_response_02;
+	public AudioClip Stone_Correct_response_03;
+
+	public AudioClip FlowerIntro;
+	public AudioClip Flower_Correct_response_01;
+
+	public AudioClip BirdIntro_afterStone;
+	public AudioClip BirdIntro_afterFlower;
+	public AudioClip Bird_Correct_response_01;
+	public AudioClip Bird_Correct_response_02;
+	public AudioClip Bird_Correct_response_03;
+
+	public AudioClip PaddleIntro;
+	public AudioClip Paddle_Correct_response_01;
+	public AudioClip Paddle_Correct_response_02;
+	public AudioClip Paddle_Correct_response_03;
+
+	public AudioClip TreeIntro;
+	public AudioClip Tree_Correct_response_01;
+	public AudioClip Tree_Correct_response_02;
+	public AudioClip Tree_Correct_response_03;
+
+	public AudioClip RopeIntro_afterTree;
+	public AudioClip RopeIntro_afterPaddle;
+	public AudioClip Rope_Correct_response_01;
+	public AudioClip Rope_Correct_response_02;
+	public AudioClip Rope_Correct_response_03;
+
+	public AudioClip BikeIntro;
+	public AudioClip Bike_Correct_response_01;
+	public AudioClip Bike_Correct_response_02;
+	public AudioClip Bike_Correct_response_03;
+
+	public AudioClip StarIntro;
+	public AudioClip Star_Correct_response_01;
+	public AudioClip Star_Correct_response_02;
+	public AudioClip Star_Correct_response_03;
+	
+	// Gesture Vocal Instruction
+	public AudioClip StoneGesture;
+	public AudioClip Stone_hint_01;
+	public AudioClip FlowerGesture;
+	public AudioClip Flower_hint_01;
+	public AudioClip BirdGesture;
+	public AudioClip Bird_hint_01;
+	public AudioClip PaddleGesture;
+	public AudioClip Paddle_hint_01;
+	public AudioClip TreeGesture;
+	public AudioClip Tree_hint_01;
+	public AudioClip RopeGesture;
+	public AudioClip Rope_hint_01;
+	public AudioClip BikeGesture;
+	public AudioClip Bike_hint_01;
+	public AudioClip StarGesture;
+	public AudioClip Star_hint_01;
+	
 	private AudioSource audiosource;
 	
 	void Awake ()
